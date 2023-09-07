983889b34f322e0a2e31d21e35b9b053d627bb50
Ryan Li <jijizaza999@gmail.com>
2023-09-07 15:59:51 -0700

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName ="Ryan";
+let lastName = "Li";
+var age = "3";
 // Exercise 2
+lastName ="Lee";
+age="4"
 
 // Exercise 3
+let language = "JavaScript";
+let createYear = 1995;
+let isCaseSensitive = true;
+
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
